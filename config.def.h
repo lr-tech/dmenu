41f961b16652c30bc57b5e7d1fd3d644426b8f0b
lr-tech <lrtech8@gmail.com>
2022-03-23 02:11:29 -0400

    Retab. Shortcuts updated. Prompt adjusted on vertical view. Tokyonight colorscheme

--- config.def.h
+++ config.def.h
@@ -1,24 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"Iosevka Nerd Font:size=10"
+static int topbar = 1;                  /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                /* -c option; centers dmenu on screen */
+static int min_width = 400;             /* minimum width when centered */
+static const char *fonts[] = {          /* -fn option overrides fonts[0]; default X11 font or font set */
+	"Iosevka Nerd Font Mono:size=12:antialias=true:autohint=true"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;       /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+                        /*     fg         bg    */
+        [SchemeNorm]   = { "#c0caf5", "#1a1b26" },
+        [SchemeSel]    = { "#1a1b26", "#f7768e" },
+        [SchemeOut]    = { "#c0caf5", "#414868" },
+        [SchemeBorder] = { "#24283b", "#24283b" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 0;
+static unsigned int lines = 6;          /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lineheight = 32;    /* -h option; minimum height of a menu line */
 static unsigned int min_lineheight = 8;
 
 /*
@@ -27,5 +25,4 @@ static unsigned int min_lineheight = 8;
  */
 static const char worddelimiters[] = " ";
 
-/* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 1;   /* Size of the window border */
