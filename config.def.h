0d9730eb958b81722f2c4cef2d7040bccdf22212
lr-tech <lrtech8@gmail.com>
2022-03-23 15:11:21 -0400

    Patch: allow-color-font-5.0 (color emoji support)

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@ static int topbar = 1;                  /* -b  option; if 0, dmenu appears at bo
 static int centered = 1;                /* -c option; centers dmenu on screen */
 static int min_width = 400;             /* minimum width when centered */
 static const char *fonts[] = {          /* -fn option overrides fonts[0]; default X11 font or font set */
-	"Iosevka Nerd Font Mono:size=12:antialias=true:autohint=true"
+	"Iosevka Nerd Font Mono:size=12:antialias=true:autohint=true",
+        "Noto Color Emoji:size=12:antialias=true:autohint=true"
 };
 static const char *prompt = NULL;       /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
